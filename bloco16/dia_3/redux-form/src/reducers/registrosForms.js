9165697976fecc9e8373e75ae550ffab75dd40a9
marcelo ivan <sagacello@hotmail.com>
2020-12-10 20:42:47 -0300

    funcionando

--- bloco16/dia_3/redux-form/src/reducers/registrosForms.js
+++ bloco16/dia_3/redux-form/src/reducers/registrosForms.js
@@ -1,18 +1,18 @@
 // aqui fica o reducer especifico do componente
 // cada action trata um reducer
 
-const ESTADO_INICIAL = {
+const ESTADO_INICIAL = [{
   nome: 'Asdrubal',
   email: 'asdrubal_cola_pinga@trago.com',
   cpf: 123456789,
-};
+}];
 
 function registerReducer(state = ESTADO_INICIAL, action) {
   switch (action.type) {
     case 'ADD_FORM':
       return [
         ...state,
-        {email: action.email, nome: action.nome, cpf: action.cpf} ,
+        { nome:action.form.nome, email:action.form.cpf, cpf:action.form.email }
       ];
 
     default:
